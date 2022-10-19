9f0d8d509218a6fad7c4ddad23c35f1e3a6f18c5
Martin Schelldorfer <ms@e-switch.ch>
2022-10-19 12:04:57 +0200

    formatter

--- src/main/java/ch/eswitch/tinylog/writers/MaskedRollingFileWriter.java
+++ src/main/java/ch/eswitch/tinylog/writers/MaskedRollingFileWriter.java
@@ -1,5 +1,15 @@
 package ch.eswitch.tinylog.writers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
 import org.tinylog.Level;
 import org.tinylog.configuration.ServiceLoader;
@@ -15,13 +25,6 @@ import org.tinylog.runtime.RuntimeProvider;
 import org.tinylog.writers.AbstractFormatPatternWriter;
 import org.tinylog.writers.raw.ByteArrayWriter;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
-
 /**
  * tinylog 2 Masked {@link org.tinylog.writers.RollingFileWriter}<br/>
  * This is a <a href="https://tinylog.org/v2/extending/#custom-writer">custom writer</a> for
@@ -30,7 +33,8 @@ import java.util.*;
  *
  * @author Martin Schelldorfer, 2022
  */
-public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
+public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter
+{
 
     private final DynamicPath path;
     private final List<Policy> policies;
@@ -46,25 +50,22 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     private final MaskedWriterUtil maskedWriter;
 
     /**
-     * @throws IOException
-     *             Log file cannot be opened for write access
-     * @throws IllegalArgumentException
-     *             A property has an invalid value or is missing in configuration
+     * @throws IOException Log file cannot be opened for write access
+     * @throws IllegalArgumentException A property has an invalid value or is missing in configuration
      */
-    public MaskedRollingFileWriter() throws IOException {
-        this(Collections.<String, String>emptyMap());
+    public MaskedRollingFileWriter() throws IOException
+    {
+        this(Collections.<String, String> emptyMap());
     }
 
     /**
-     * @param properties
-     *            Configuration for writer
+     * @param properties Configuration for writer
      *
-     * @throws IOException
-     *             Log file cannot be opened for write access
-     * @throws IllegalArgumentException
-     *             A property has an invalid value or is missing in configuration
+     * @throws IOException Log file cannot be opened for write access
+     * @throws IllegalArgumentException A property has an invalid value or is missing in configuration
      */
-    public MaskedRollingFileWriter(final Map<String, String> properties) throws IOException {
+    public MaskedRollingFileWriter(final Map<String, String> properties) throws IOException
+    {
         super(properties);
 
         path = new DynamicPath(getFileName());
@@ -76,22 +77,30 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
         List<FileTuple> files = getAllFileTuplesWithoutLinks(converter.getBackupSuffix());
         File latestFile = findLatestLogFile(files);
 
-        if (backups >= 0) {
+        if (backups >= 0)
+        {
             deleteBackups(files, backups);
         }
 
         String fileName;
         boolean append;
 
-        if (latestFile != null && path.isValid(latestFile)) {
+        if (latestFile != null
+                && path.isValid(latestFile))
+        {
             fileName = latestFile.getAbsolutePath();
-            if (canBeContinued(fileName, policies)) {
+            if (canBeContinued(fileName, policies))
+            {
                 append = true;
-            } else {
+            }
+            else
+            {
                 fileName = path.resolve();
                 append = false;
             }
-        } else {
+        }
+        else
+        {
             fileName = path.resolve();
             append = false;
         }
@@ -105,36 +114,51 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     }
 
     @Override
-    public void write(LogEntry logEntry) throws IOException {
+    public void write(LogEntry logEntry) throws IOException
+    {
         logEntry = maskedWriter.mask(logEntry);
 
         byte[] data = render(logEntry).getBytes(charset);
-        if (writingThread) {
+        if (writingThread)
+        {
             internalWrite(data);
-        } else {
-            synchronized (writer) {
+        }
+        else
+        {
+            synchronized (writer)
+            {
                 internalWrite(data);
             }
         }
     }
 
     @Override
-    public void flush() throws IOException {
-        if (writingThread) {
+    public void flush() throws IOException
+    {
+        if (writingThread)
+        {
             internalFlush();
-        } else {
-            synchronized (writer) {
+        }
+        else
+        {
+            synchronized (writer)
+            {
                 internalFlush();
             }
         }
     }
 
     @Override
-    public void close() throws IOException, InterruptedException {
-        if (writingThread) {
+    public void close() throws IOException, InterruptedException
+    {
+        if (writingThread)
+        {
             internalClose();
-        } else {
-            synchronized (writer) {
+        }
+        else
+        {
+            synchronized (writer)
+            {
                 internalClose();
             }
         }
@@ -143,24 +167,26 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Outputs a passed byte array unsynchronized.
      *
-     * @param data
-     *            Byte array to output
-     * @throws IOException
-     *             Writing failed
+     * @param data Byte array to output
+     * @throws IOException Writing failed
      */
-    private void internalWrite(final byte[] data) throws IOException {
-        if (!canBeContinued(data, policies)) {
+    private void internalWrite(final byte[] data) throws IOException
+    {
+        if (!canBeContinued(data, policies))
+        {
             writer.close();
             converter.close();
 
             String fileName = path.resolve();
             writer = createByteArrayWriterAndLinkLatest(fileName, false, buffered, charset);
 
-            for (Policy policy : policies) {
+            for (Policy policy : policies)
+            {
                 policy.reset();
             }
 
-            if (backups >= 0) {
+            if (backups >= 0)
+            {
                 deleteBackups(getAllFileTuplesWithoutLinks(converter.getBackupSuffix()), backups);
             }
         }
@@ -172,22 +198,21 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Outputs buffered log entries immediately unsynchronized.
      *
-     * @throws IOException
-     *             Flushing failed
+     * @throws IOException Flushing failed
      */
-    private void internalFlush() throws IOException {
+    private void internalFlush() throws IOException
+    {
         writer.flush();
     }
 
     /**
      * Closes the writer unsynchronized.
      *
-     * @throws IOException
-     *             Closing failed
-     * @throws InterruptedException
-     *             Interrupted while waiting for the converter
+     * @throws IOException Closing failed
+     * @throws InterruptedException Interrupted while waiting for the converter
      */
-    private void internalClose() throws IOException, InterruptedException {
+    private void internalClose() throws IOException, InterruptedException
+    {
         writer.close();
         converter.close();
         converter.shutdown();
@@ -196,18 +221,23 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Gets all log files including backups from {@link DynamicPath} but without links.
      *
-     * @param backupSuffix
-     *            File extension for backup files
+     * @param backupSuffix File extension for backup files
      * @return Found file tuples without links
      */
     @IgnoreJRERequirement
-    private List<FileTuple> getAllFileTuplesWithoutLinks(final String backupSuffix) {
+    private List<FileTuple> getAllFileTuplesWithoutLinks(final String backupSuffix)
+    {
         List<FileTuple> files = path.getAllFiles(backupSuffix);
-        if (linkToLatest != null && !RuntimeProvider.isAndroid()) {
+        if (linkToLatest != null
+                && !RuntimeProvider.isAndroid())
+        {
             File fileLink = new File(linkToLatest.resolve()).getAbsoluteFile();
             Iterator<FileTuple> iterator = files.iterator();
-            while (iterator.hasNext()) {
-                if (fileLink.equals(iterator.next().getOriginal())) {
+            while (iterator.hasNext())
+            {
+                if (fileLink.equals(iterator.next()
+                                            .getOriginal()))
+                {
                     iterator.remove();
                     break;
                 }
@@ -219,36 +249,37 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Creates a {@link ByteArrayWriter} for a file and creates a link to it if linking is enabled.
      *
-     * @param fileName
-     *            Name of file to open for writing
-     * @param append
-     *            An already existing file should be continued
-     * @param buffered
-     *            Output should be buffered
-     * @param charset
-     *            Charset used by the writer
+     * @param fileName Name of file to open for writing
+     * @param append An already existing file should be continued
+     * @param buffered Output should be buffered
+     * @param charset Charset used by the writer
      * @return Writer for writing to passed file
-     * @throws IOException
-     *             Log file cannot be opened for write access
+     * @throws IOException Log file cannot be opened for write access
      */
     @IgnoreJRERequirement
-    private ByteArrayWriter createByteArrayWriterAndLinkLatest(final String fileName, final boolean append,
-                                                               final boolean buffered, final Charset charset) throws IOException {
+    private ByteArrayWriter createByteArrayWriterAndLinkLatest(final String fileName, final boolean append, final boolean buffered, final Charset charset) throws IOException
+    {
         converter.open(fileName);
         ByteArrayWriter writer = createByteArrayWriter(fileName, append, buffered, false, false, charset);
-        if (linkToLatest != null) {
+        if (linkToLatest != null)
+        {
             File logFile = new File(fileName);
             File linkFile = new File(linkToLatest.resolve());
-            if (!RuntimeProvider.isAndroid()) {
-                try {
+            if (!RuntimeProvider.isAndroid())
+            {
+                try
+                {
                     Path logPath = logFile.toPath();
                     Path linkPath = linkFile.toPath();
                     Files.deleteIfExists(linkPath);
                     Files.createLink(linkPath, logPath);
-                } catch (IOException ex) {
+                } catch (IOException ex)
+                {
                     InternalLogger.log(Level.ERROR, ex, "Failed to create link '" + linkFile + "'");
                 }
-            } else {
+            }
+            else
+            {
                 InternalLogger.log(Level.WARN, "Cannot create link to latest log file on Android");
             }
         }
@@ -258,13 +289,20 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Finds the latest existing original log file.
      *
-     * @param files
-     *            All original and backup files
+     * @param files All original and backup files
      * @return Found original log file or {@code null} if there are no original log files
      */
-    private static File findLatestLogFile(final List<FileTuple> files) {
-        for (FileTuple file : files) {
-            if (file.getOriginal().isFile() && (file.getOriginal().equals(file.getBackup()) || !file.getBackup().isFile())) {
+    private static File findLatestLogFile(final List<FileTuple> files)
+    {
+        for (FileTuple file : files)
+        {
+            if (file.getOriginal()
+                    .isFile()
+                    && (file.getOriginal()
+                            .equals(file.getBackup())
+                            || !file.getBackup()
+                                    .isFile()))
+            {
                 return file.getOriginal();
             }
         }
@@ -275,15 +313,20 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Creates policies from a nullable string.
      *
-     * @param property
-     *            Nullable string with policies to create
+     * @param property Nullable string with policies to create
      * @return Created policies
      */
-    private static List<Policy> createPolicies(final String property) {
-        if (property == null || property.isEmpty()) {
-            return Collections.<Policy>singletonList(new StartupPolicy(null));
-        } else {
-            if (RuntimeProvider.getProcessId() == Long.MIN_VALUE) {
+    private static List<Policy> createPolicies(final String property)
+    {
+        if (property == null
+                || property.isEmpty())
+        {
+            return Collections.<Policy> singletonList(new StartupPolicy(null));
+        }
+        else
+        {
+            if (RuntimeProvider.getProcessId() == Long.MIN_VALUE)
+            {
                 java.util.ServiceLoader.load(Policy.class); // Workaround for ProGuard (see issue #126)
             }
 
@@ -294,15 +337,20 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Creates the file converter from a nullable string.
      *
-     * @param property
-     *            Nullable string with converter to create
+     * @param property Nullable string with converter to create
      * @return Created file converter
      */
-    private static FileConverter createConverter(final String property) {
-        if (property == null || property.isEmpty()) {
+    private static FileConverter createConverter(final String property)
+    {
+        if (property == null
+                || property.isEmpty())
+        {
             return new NopFileConverter();
-        } else {
-            if (RuntimeProvider.getProcessId() == Long.MIN_VALUE) {
+        }
+        else
+        {
+            if (RuntimeProvider.getProcessId() == Long.MIN_VALUE)
+            {
                 java.util.ServiceLoader.load(FileConverter.class); // Workaround for ProGuard (see issue #126)
             }
 
@@ -314,15 +362,15 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Checks if an already existing log file can be continued.
      *
-     * @param fileName
-     *            Log file
-     * @param policies
-     *            Policies that should be applied
+     * @param fileName Log file
+     * @param policies Policies that should be applied
      * @return {@code true} if the passed log file can be continued, {@code false} if a new log file should be started
      */
-    private static boolean canBeContinued(final String fileName, final List<Policy> policies) {
+    private static boolean canBeContinued(final String fileName, final List<Policy> policies)
+    {
         boolean result = true;
-        for (Policy policy : policies) {
+        for (Policy policy : policies)
+        {
             result &= policy.continueExistingFile(fileName);
         }
         return result;
@@ -331,15 +379,15 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Checks if a new log entry can be still written to the current log file.
      *
-     * @param data
-     *            Log entry
-     * @param policies
-     *            Policies that should be applied
+     * @param data Log entry
+     * @param policies Policies that should be applied
      * @return {@code true} if the current log file can be continued, {@code false} if a new log file should be started
      */
-    private static boolean canBeContinued(final byte[] data, final List<Policy> policies) {
+    private static boolean canBeContinued(final byte[] data, final List<Policy> policies)
+    {
         boolean result = true;
-        for (Policy policy : policies) {
+        for (Policy policy : policies)
+        {
             result &= policy.continueCurrentFile(data);
         }
         return result;
@@ -348,16 +396,16 @@ public final class MaskedRollingFileWriter extends AbstractFormatPatternWriter {
     /**
      * Deletes old log files.
      *
-     * @param files
-     *            All original and backup files
-     * @param count
-     *            Number of log files to keep
+     * @param files All original and backup files
+     * @param count Number of log files to keep
      */
-    private static void deleteBackups(final List<FileTuple> files, final int count) {
-        for (int i = count; i < files.size(); ++i) {
-            files.get(i).delete();
+    private static void deleteBackups(final List<FileTuple> files, final int count)
+    {
+        for (int i = count; i < files.size(); ++i)
+        {
+            files.get(i)
+                 .delete();
         }
     }
 
 }
-
